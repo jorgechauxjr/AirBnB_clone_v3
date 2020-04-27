25c9a692eea731d3b31f1192ea4bec61c29c21c0
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-27 22:47:24 +0000

    Task 4 done, starting of api rest with flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""Flask server module"""
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+# app_views variable created in api.v1.views
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exc):
+    """Teardown db"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    import os
+    app.config['ENV'] = 'development'
+    host = os.getenv("HBNB_API_HOST", '0.0.0.0')
+    port = os.getenv("HBNB_API_PORT", '5000')
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""Index file of views package"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status")
+def get_status():
+    """Get status from /status"""
+
+    return jsonify(status="OK")
