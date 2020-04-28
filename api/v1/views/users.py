4e4440f984f75f6eb263339deadb46c9dc2a5a64
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-28 23:04:38 +0000

    Fix status code of get States and created crud of users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,1 +1,55 @@
 #!/usr/bin/python3
+
+"""Users module of view package"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+
+
+@app_views.route("/users", methods=["GET", "POST"])
+def get_users():
+    """Get all users"""
+
+    if request.method == "GET":
+        objs = storage.all("User")
+        all_users = [obj.to_dict() for obj in objs.values()]
+        return jsonify(all_users)
+
+    if request.method == "POST":
+        if request.is_json is False:
+            abort(400, "Not a JSON")
+        user_dict = request.get_json()
+        if "email" not in user_dict:
+            abort(400, "Missing email")
+        if "password" not in user_dict:
+            abort(400, "Missing password")
+        new_user = User(**user_dict)
+        new_user.save()
+        return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route("/users/<user_id>", methods=["GET", "DELETE", "PUT"])
+def handle_user(user_id=None):
+    """Get, Delete or Update a user by id"""
+
+    obj_user = storage.get("User", user_id)
+
+    if obj_user is None:
+        abort(404)
+
+    if request.method == "GET":
+        return jsonify(obj_user.to_dict())
+
+    if request.method == "DELETE":
+        obj_user.delete()
+        storage.save()
+        return jsonify({})
+
+    if request.method == "PUT":
+        if request.is_json is False:
+            abort(400, "Not a JSON")
+        user_dict = request.get_json()
+        obj_user.update(user_dict)
+        return jsonify(obj_user.to_dict())
