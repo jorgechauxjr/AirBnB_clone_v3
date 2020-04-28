dbd2bc2a3586e1d41135c2c0e7d781691b842658
jorgechauxjr <chauxjorge@hotmail.com>
2020-04-28 23:08:35 +0000

    Add 9-amenities GET /amenities by ID

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -14,3 +14,12 @@ def all_amenities():
         amenities_list.append(element.to_dict())
     return jsonify(amenities_list)
 
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def amenities_list_id(amenities_id):
+    """Retrieves an Amenity object by Id"""
+    amenities_objs = storage.all('Amenity').values()
+    for element in amenities_objs:
+        if element.id == amenities_id:
+            return jsonify(element.to_dict())
+    abort(404)
