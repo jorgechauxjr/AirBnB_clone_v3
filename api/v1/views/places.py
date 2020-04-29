ed43b4bcc030a2f94d677515c650b0ee978e4fcd
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-29 01:25:00 +0000

    Created endpoint to get a place by city

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,38 +8,39 @@ from models import storage
 from models.place import Place
 
 
-@app_views.route("/places", methods=["GET"])
-def get_places():
-    """Get all places"""
-
-    objs = storage.all("Place")
-    all_places = [obj.to_dict() for obj in objs.values()]
-    return jsonify(all_places)
-
-
-@app_views.route("/cities/<city_id>/places", methods=["POST"])
-def create_place(city_id=None):
-    """Cretes a place given a city id"""
+@app_views.route("/cities/<city_id>/places", methods=["GET", "POST"])
+def create_or_get_place(city_id=None):
+    """Creates or get a place given a city id"""
 
     obj_city = storage.get("City", city_id)
 
     if obj_city is None:
         abort(404)
 
-    if request.is_json is False:
-        abort(400, "Not a JSON")
-    place_dict = request.get_json()
-    if "user_id" not in place_dict:
-        abort(400, "Missing user_id")
-    obj_user = storage.get("User", place_dict.get('user_id'))
-    if obj_user is None:
-        abort(404)
-    if "name" not in place_dict:
-        abort(400, "Missing name")
-    place_dict.update(city_id=city_id)
-    new_place = Place(**place_dict)
-    new_place.save()
-    return jsonify(new_place.to_dict()), 201
+    if request.method == "GET":
+        objs = storage.all("Place")
+        place_by_city = []
+        for obj in objs.values():
+            obj_dict = obj.to_dict()
+            if obj_dict.get('city_id') == city_id:
+                place_by_city.append(obj_dict)
+        return jsonify(place_by_city)
+
+    if request.method == "POST":
+        if request.is_json is False:
+            abort(400, "Not a JSON")
+        place_dict = request.get_json()
+        if "user_id" not in place_dict:
+            abort(400, "Missing user_id")
+        obj_user = storage.get("User", place_dict.get('user_id'))
+        if obj_user is None:
+            abort(404)
+        if "name" not in place_dict:
+            abort(400, "Missing name")
+        place_dict.update(city_id=city_id)
+        new_place = Place(**place_dict)
+        new_place.save()
+        return jsonify(new_place.to_dict()), 201
 
 
 @app_views.route("/places/<place_id>", methods=["GET", "DELETE", "PUT"])
